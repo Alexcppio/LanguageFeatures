878e5f444a81d6c978eddfd31742952e92553c4a
Alexcppio <62553154+Alexcppio@users.noreply.github.com>
2021-09-25 13:27:19 +0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ViewResult> Index()
+        {
+            List<string> output = new List<string>();
+            foreach (long? len in await MyAsyncMethods.GetPageLengths(output, "apress.com", "microsoft.com", "amazon.com"))
+            {
+                output.Add($"Page length: {len}"); // Длина страницы
+            }
+            return View(output);
+        }
+    }
+}
